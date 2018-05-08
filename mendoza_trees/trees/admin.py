fd3ff53648fa363830a7cf2d355a8c1b0d14a0f7
Simon Willison <simonw@eventbrite.com>
2018-05-08 13:56:15 -0300

    Configured Django admin

--- mendoza_trees/trees/admin.py
+++ mendoza_trees/trees/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Tree, Species
+
+admin.site.register(Tree,
+    list_display=('species', 'latitude', 'longitude')
+)
+admin.site.register(Species)
